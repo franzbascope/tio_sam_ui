259a18c47d1585447046128b71bcacff1575f476
Franz Bascope <franzbascope123@gmail.com>
2020-07-03 13:02:13 -0400

    separate table

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,7 +1,8 @@
 import React, { useEffect, useState } from "react";
-import { Table, Breadcrumb, Button, Alert } from "react-bootstrap";
+import { Breadcrumb, Button, Alert } from "react-bootstrap";
 import axios from "axios";
 import { API_URL } from "../../configuration/index";
+import Table from "./table";
 
 export default () => {
   const [inputValues, setValues] = useState({
@@ -78,45 +79,12 @@ export default () => {
         ""
       )}
       {inputValues.products.length > 0 ? (
-        <div className="table-responsive">
-          <Table striped bordered hover>
-            <thead>
-              <tr>
-                <th>Name</th>
-                <th>Price</th>
-                <th>Weight</th>
-                <th>Cost</th>
-                <th>Actions</th>
-              </tr>
-            </thead>
-            <tbody>
-              {inputValues.products.map((product, key) => {
-                const { price, weight, cost, name, _id } = product;
-                return (
-                  <tr key>
-                    <td>{name}</td>
-                    <td>{price}</td>
-                    <td>{weight}</td>
-                    <td>{cost}</td>
-                    <td>
-                      <Button variant="success" className="ml-3 mr-3">
-                        Edit
-                      </Button>
-                      <Button
-                        variant="danger"
-                        onClick={() => {
-                          deleteProduct(_id);
-                        }}
-                      >
-                        Delete
-                      </Button>
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </Table>
-        </div>
+        <Table
+          products={inputValues.products}
+          deleteProduct={(id) => {
+            deleteProduct(id);
+          }}
+        />
       ) : (
         <Alert variant="warning">No products registered, Add one !!!</Alert>
       )}
---
+++ src/pages/products/table.js
@@ -0,0 +1,46 @@
+import React from "react";
+import { Table, Button } from "react-bootstrap";
+
+export default ({ products, deleteProduct }) => {
+  return (
+    <div className="table-responsive">
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>Name</th>
+            <th>Price</th>
+            <th>Weight</th>
+            <th>Cost</th>
+            <th>Actions</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((product, key) => {
+            const { price, weight, cost, name, _id } = product;
+            return (
+              <tr key>
+                <td>{name}</td>
+                <td>{price}</td>
+                <td>{weight}</td>
+                <td>{cost}</td>
+                <td>
+                  <Button variant="success" className="ml-3 mr-3">
+                    Edit
+                  </Button>
+                  <Button
+                    variant="danger"
+                    onClick={() => {
+                      deleteProduct(_id);
+                    }}
+                  >
+                    Delete
+                  </Button>
+                </td>
+              </tr>
+            );
+          })}
+        </tbody>
+      </Table>
+    </div>
+  );
+};
