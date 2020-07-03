7761df44c6d677b35b1ea62b66a85cae907a6fba
Franz Bascope <franzbascope123@gmail.com>
2020-07-03 12:52:30 -0400

    add delete product

--- src/pages/products/index.js
+++ src/pages/products/index.js
@@ -1,11 +1,13 @@
 import React, { useEffect, useState } from "react";
-import { Table, Breadcrumb, Button } from "react-bootstrap";
+import { Table, Breadcrumb, Button, Alert } from "react-bootstrap";
 import axios from "axios";
 import { API_URL } from "../../configuration/index";
 
 export default () => {
   const [inputValues, setValues] = useState({
     products: [],
+    error: null,
+    success: null,
   });
 
   useEffect(() => {
@@ -14,11 +16,29 @@ export default () => {
         let res = await axios.get(`${API_URL}/products`);
         setValues({ ...inputValues, products: res.data.data });
       } catch (e) {
-        alert("Error al obtener datos" + e);
+        setValues({ inputValues, error: "Errot fetching products: " + e });
       }
     };
     getProducts();
-  });
+  }, []);
+
+  const deleteProduct = async (id) => {
+    try {
+      if (window.confirm("Are you sure you want to delete this product?")) {
+        await axios.delete(`${API_URL}/products/${id}`);
+        let products = inputValues.products.filter((product) => {
+          return product._id != id;
+        });
+        setValues({
+          ...inputValues,
+          products,
+          success: "Product deleted successfuly: ",
+        });
+      }
+    } catch (e) {
+      setValues({ inputValues, error: "Error deleting product: " + e });
+    }
+  };
 
   return (
     <React.Fragment>
@@ -28,38 +48,78 @@ export default () => {
       <Button variant="primary" className="mt-3 mb-3">
         New Product
       </Button>
-      <div className="table-responsive">
-        <Table striped bordered hover>
-          <thead>
-            <tr>
-              <th>Name</th>
-              <th>Price</th>
-              <th>Weight</th>
-              <th>Cost</th>
-              <th>Actions</th>
-            </tr>
-          </thead>
-          <tbody>
-            {inputValues.products.map((product, key) => {
-              const { price, weight, cost, name } = product;
-              return (
-                <tr key>
-                  <td>{name}</td>
-                  <td>{price}</td>
-                  <td>{weight}</td>
-                  <td>{cost}</td>
-                  <td>
-                    <Button variant="success" className="ml-3 mr-3">
-                      Edit
-                    </Button>
-                    <Button variant="danger">Delete</Button>
-                  </td>
-                </tr>
-              );
-            })}
-          </tbody>
-        </Table>
-      </div>
+      {inputValues.error ? (
+        <Alert
+          onClose={() =>
+            setValues({ ...inputValues, success: null, error: null })
+          }
+          className="mt-3 mb-3"
+          dismissible
+          variant="danger"
+        >
+          {inputValues.error}
+        </Alert>
+      ) : (
+        ""
+      )}
+
+      {inputValues.success ? (
+        <Alert
+          onClose={() =>
+            setValues({ ...inputValues, success: null, error: null })
+          }
+          className="mt-3 mb-3"
+          dismissible
+          variant="success"
+        >
+          {inputValues.success}
+        </Alert>
+      ) : (
+        ""
+      )}
+      {inputValues.products.length > 0 ? (
+        <div className="table-responsive">
+          <Table striped bordered hover>
+            <thead>
+              <tr>
+                <th>Name</th>
+                <th>Price</th>
+                <th>Weight</th>
+                <th>Cost</th>
+                <th>Actions</th>
+              </tr>
+            </thead>
+            <tbody>
+              {inputValues.products.map((product, key) => {
+                const { price, weight, cost, name, _id } = product;
+                return (
+                  <tr key>
+                    <td>{name}</td>
+                    <td>{price}</td>
+                    <td>{weight}</td>
+                    <td>{cost}</td>
+                    <td>
+                      <Button variant="success" className="ml-3 mr-3">
+                        Edit
+                      </Button>
+                      <Button
+                        variant="danger"
+                        onClick={() => {
+                          deleteProduct(_id);
+                        }}
+                      >
+                        Delete
+                      </Button>
+                    </td>
+                  </tr>
+                );
+              })}
+            </tbody>
+          </Table>
+        </div>
+      ) : (
+        <Alert variant="warning">No products registered, Add one !!!</Alert>
+      )}
     </React.Fragment>
   );
 };
