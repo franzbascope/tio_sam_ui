a2cb2b4466b96c9f5567354f45bd7e9a78260759
Samuel Cordova <samycorbal@gmail.com>
2020-08-14 16:16:53 -0400

    Add new globalValues in request handler

--- src/shared/requestHandler.js
+++ src/shared/requestHandler.js
@@ -35,6 +35,7 @@ export default () => {
           res = await axios.delete(`${API_URL}/${url}/${id}`, config);
           break;
         case "PAGE":
+          if (!id) id = 1;
           res = await axios.get(`${API_URL}/${url}/page/${id}`, config);
           break;
       }
@@ -59,6 +60,13 @@ export default () => {
     setGlobalValues({ ...globalValues, loading: false });
     if (method != "GET") {
       setGlobalValues({ ...globalValues, success: message });
+    }
+    if( method == "PAGE"){
+      setGlobalValues({
+        ...globalValues,
+        totalPages: res.data.totalPages,
+        currentPage: id,
+      });
     }
     return res;
   };
