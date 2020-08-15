d01daaf8254365045f0455aa2cdb31613f8b540b
Franz Bascope <franzbascope123@gmail.com>
2020-08-14 21:26:50 -0400

    change url and add searh component

--- src/shared/urls.js
+++ src/shared/urls.js
@@ -1,6 +1,6 @@
-//https://devtiosam.herokuapp.com
+// export const mainUrl = "https://devtiosam.herokuapp.com"
 //http://localhost:3000
-export const mainUrl = "http://localhost:3000";
+export const mainUrl = "https://devtiosam.herokuapp.com";
 export const productsUrl = "products";
 export const buysUrl = "buys";
 export const loginUrl = "auth/login";
